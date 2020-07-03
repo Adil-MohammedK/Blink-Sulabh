f91525617e13fee3ec1adf6cdba50c08e8f0cb86
Adil-MohammedK <Adil-MohammedK@github.com>
2020-07-03 20:27:57 +0530

    Added TTS on Touch Screens

--- static/js/text2speech.js
+++ static/js/text2speech.js
@@ -1,5 +1,7 @@
 var available_voices;
-
+var TTS_Power = localStorage.getItem('TTS_Power');
+console.log('TTS Power on start: ' + TTS_Power);
+if (TTS_Power == 'On') textToSpeechPower();
 if (window.speechSynthesis.getVoices().length == 0) {
   window.speechSynthesis.addEventListener('voiceschanged', function () {
     getVoiceList();
@@ -41,23 +43,33 @@ $('#speak-button').on('click', function () {
 function textToSpeechPower() {
   if (document.getElementById('power').innerHTML == 'Off') {
     document.getElementById('power').innerHTML = 'On';
+    console.log('TTS Power On');
+    localStorage.setItem('TTS_Power', 'On');
   } else {
     document.getElementById('power').innerHTML = 'Off';
+    console.log('TTS power Off');
+    localStorage.setItem('TTS_Power', 'Off');
   }
 }
 
 //text to speech based on mouse coodinates
-function showCoords(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-
-  var coords = 'X coords: ' + x + ', Y coords: ' + y;
+function showCoords(event, type) {
+  var x, y, coords, elementMouseIsOver;
+  console.log('Event: ' + event);
+  if (type == 'Hover') {
+    x = event.clientX;
+    y = event.clientY;
+  } else if (type == 'Touch') {
+    x = event.touches[0].clientX;
+    y = event.touches[0].clientY;
+  }
+  coords = 'X coords: ' + x + ', Y coords: ' + y;
   // document.getElementById("coords").innerHTML = coords;
-  var elementMouseIsOver = document.elementFromPoint(x, y);
+  elementMouseIsOver = document.elementFromPoint(x, y);
 
   // document.getElementById("elementName").innerHTML = elementMouseIsOver.innerText;
-
-  var text = elementMouseIsOver.innerText;
+  text = elementMouseIsOver.innerText;
+  // else if (type == 'Touch')
 
   if ((text != '') & (document.getElementById('power').innerHTML == 'On')) {
     var utter = new SpeechSynthesisUtterance();
@@ -68,11 +80,29 @@ function showCoords(event) {
     var synth = window.speechSynthesis;
     synth.speak(utter);
   }
+  if (type == 'Hover') {
+    document
+      .elementFromPoint(x, y)
+      .addEventListener('mouseout', function (event) {
+        event.preventDefault();
+        window.speechSynthesis.cancel();
+      });
+  } else if (type == 'Touch') {
+    document
+      .elementFromPoint(x, y)
+      .addEventListener('touchend', function (event) {
+        event.preventDefault();
+        window.speechSynthesis.cancel();
+      });
+  }
+}
 
-  document
-    .elementFromPoint(x, y)
-    .addEventListener('mouseout', function (event) {
-      event.preventDefault();
-      window.speechSynthesis.cancel();
-    });
+function addTTS(body) {
+  document.querySelectorAll('*').forEach(function (node) {
+    var attr = document.createAttribute('ontouchmove');
+    var text = node.innerText;
+    attr.value = "showCoords(event,'Touch',text)";
+    node.setAttribute(attr);
+  });
+  console.log('TTS touchmove added');
 }
