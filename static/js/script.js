7bda695ffe664851ba5ea830bf5ddf6d1aaef2aa
Adil-MohammedK <adilmohammed2000@outlook.com>
2020-07-27 17:06:33 +0530

    Put findText into script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,11 @@
-var body = '',
-  head = '',
-  html = '';
+var images = document.getElementsByTagName('img');
+// To change hrefs of img and add rural.nic.in to it
+for (var i = 0; i < images.length; i++) {
+  if (images[i].src.includes(window.origin)) {
+    str = images[i].src.split('https://rural.nic.in/').pop();
+    images[i].src = 'https://rural.nic.in/' + str.split(window.origin).pop();
+  }
+}
 document.getElementById('loader').style.visibility = 'hidden';
 $(document).ready(function () {
   url = window.location.href;
@@ -27,9 +32,38 @@ $(document).ready(function () {
         console.log(data);
         html = data.html;
         console.log(html);
+        linkData = findLinks(html);
+        console.log(linkData);
+        imgData = findLinks(html);
+        console.log(imgData);
       });
     })
     .catch(function (error) {
       console.log('Fetch error: ' + error);
     });
 });
+
+$(document).ready(function () {
+  $("a[href='/']").attr('href', '/?name=home'); //To change links of home to custom home link
+});
+function findLinks(htmlString) {
+  var $container = $('<div/>').html(htmlString);
+
+  //links function
+  links = []; //return in function
+  $container.find('a').each(function () {
+    links.push([this.innerText, this.href);
+  });
+  return links;
+}
+
+function findImages(htmlString) {
+  var $container = $('<div/>').html(htmlString); //necessary for all functions
+
+  //images function
+  imgs = []; //return in function
+  $container.find('img').each(function () {
+    imgs.push([this.src, this.alt]);
+  });
+  return imgs;
+}
