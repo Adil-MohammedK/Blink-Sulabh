523d9aa8dc34c6936d3a1fd116ca128921f1f0b2
Adil-MohammedK <adilmohammed2000@outlook.com>
2020-07-29 20:34:40 +0530

    Changed Sulabh Home and added option cookies

--- static/js/main.js
+++ static/js/main.js
@@ -1,47 +1,52 @@
-const navbarToggler = document.querySelector(".navbar-toggler");
-const navbarMenu = document.querySelector(".navbar ul");
-const navbarLinks = document.querySelectorAll(".navbar a");
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarMenu = document.querySelector('.navbar ul');
+const navbarLinks = document.querySelectorAll('.navbar a');
 
-navbarToggler.addEventListener("click", navbarTogglerClick);
+navbarToggler.addEventListener('click', navbarTogglerClick);
 
 function navbarTogglerClick() {
-  navbarToggler.classList.toggle("open-navbar-toggler");
-  navbarMenu.classList.toggle("open");
+  navbarToggler.classList.toggle('open-navbar-toggler');
+  navbarMenu.classList.toggle('open');
 }
 
 // navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
 
-for(let i=0; i<navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", navbarLinkClick);
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener('click', navbarLinkClick);
 }
 
 function navbarLinkClick(event) {
-
   smoothScroll(event); // Call the "smoothScroll" function
 
-  if(navbarMenu.classList.contains("open")) { // Close navbarMenu in smaller screens
+  if (navbarMenu.classList.contains('open')) {
+    // Close navbarMenu in smaller screens
     navbarToggler.click();
   }
-
 }
 
 // Smooth-Scrolling
 function smoothScroll(event) {
   event.preventDefault();
-  const targetId = event.currentTarget.getAttribute("href")==="#" ? "header" : event.currentTarget.getAttribute("href");
+  const targetId =
+    event.currentTarget.getAttribute('href') === '#'
+      ? 'header'
+      : event.currentTarget.getAttribute('href');
   const targetPosition = document.querySelector(targetId).offsetTop;
   const startPosition = window.pageYOffset;
   const distance = targetPosition - startPosition;
   const duration = 1000;
   let start = null;
-  
+
   window.requestAnimationFrame(step);
 
   function step(timestamp) {
     if (!start) start = timestamp;
     const progress = timestamp - start;
     // window.scrollTo(0, distance*(progress/duration) + startPosition);
-    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    window.scrollTo(
+      0,
+      easeInOutCubic(progress, startPosition, distance, duration)
+    );
     if (progress < duration) window.requestAnimationFrame(step);
   }
 }
@@ -49,36 +54,211 @@ function smoothScroll(event) {
 // Easing Functions
 
 function linear(t, b, c, d) {
-	return c*t/d + b;
-};
+  return (c * t) / d + b;
+}
 
 function easeInOutQuad(t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t + b;
-	t--;
-	return -c/2 * (t*(t-2) - 1) + b;
-};
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t + b;
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+}
 
 function easeInOutCubic(t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t*t + b;
-	t -= 2;
-	return c/2*(t*t*t + 2) + b;
-};
-
-/*const features = document.querySelectorAll(".feature a");
-const featureList = document.querySelectorAll (".feature_list div");
-const intro = document.querySelector("#intro")
-
-for(let j=0; j<features.length; j++) {
-  features[j].addEventListener("click", featureClick(j));
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t * t + b;
+  t -= 2;
+  return (c / 2) * (t * t * t + 2) + b;
 }
-function featureClick(j){
-  for(let k=0; k<featureList.length; k++){
-    if(k==j){
-      featureList[k].classList.toggle(.reveal);
-      featureList[k].classList.toggle(.hidden);
+
+/*const features = document.querySelectorAll(".feature");
+const options = document.querySelectorAll(".options");
+for(let i=0; i<features.length;i++){
+  features[i].addEventListener("click", function(){
+  options[i].classList.toggle = "hidden";
+});
+}*/
+
+/*var x1 = document.getElementById("voice_based_assistance");
+var x2= document.getElementById("gesture_based_navigation");
+var x3 = document.getElementById("captions_and_hyperlinks");
+var x4 = document.getElementById("text_to_braille");
+var x5 = document.getElementById("ease_of_access");
+var x6 = document.getElementById("appearance_settings");*/
+var x = document.getElementsByClassName('options');
+
+function display1() {
+  if (x[0].style.display == 'none') {
+    x[0].style.display = 'block';
+    for (let i = 0; i < x.length; i++) {
+      if (i == 0) {
+        continue;
+      } else {
+        x[i].style.display = 'none';
+      }
+    }
+  } else {
+    x[0].style.display = 'none';
+  }
+}
+function display2() {
+  if (x[1].style.display == 'none') {
+    x[1].style.display = 'block';
+    for (let i = 0; i < x.length; i++) {
+      if (i == 1) {
+        continue;
+      } else {
+        x[i].style.display = 'none';
+      }
+    }
+  } else {
+    x[1].style.display = 'none';
+  }
+}
+function display3() {
+  if (x[2].style.display == 'none') {
+    x[2].style.display = 'block';
+    for (let i = 0; i < x.length; i++) {
+      if (i == 2) {
+        continue;
+      } else {
+        x[i].style.display = 'none';
+      }
+    }
+  } else {
+    x[2].style.display = 'none';
+  }
+}
+function display4() {
+  if (x[3].style.display == 'none') {
+    x[3].style.display = 'block';
+    for (let i = 0; i < x.length; i++) {
+      if (i == 3) {
+        continue;
+      } else {
+        x[i].style.display = 'none';
+      }
+    }
+  } else {
+    x[3].style.display = 'none';
+  }
+}
+function display5() {
+  if (x[4].style.display == 'none') {
+    x[4].style.display = 'block';
+    for (let i = 0; i < x.length; i++) {
+      if (i == 4) {
+        continue;
+      } else {
+        x[i].style.display = 'none';
+      }
+    }
+  } else {
+    x[4].style.display = 'none';
+  }
+}
+function display6() {
+  if (x[5].style.display == 'none') {
+    x[5].style.display = 'block';
+    for (let i = 0; i < x.length; i++) {
+      if (i == 5) {
+        continue;
+      } else {
+        x[i].style.display = 'none';
+      }
+    }
+  } else {
+    x[5].style.display = 'none';
+  }
+}
+
+//slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function getSelectedCheckboxValues(name) {
+  const checkboxes = document.querySelectorAll(`input:checked`);
+  let values = [];
+  checkboxes.forEach((checkbox) => {
+    values.push(checkbox.value);
+  });
+  return values;
+}
+// const submitbtn = document.querySelector('#submitbtn');
+document.querySelectorAll('.submitbtn').forEach((item) => {
+  item.addEventListener('click', (event) => {
+    console.log(getSelectedCheckboxValues('color'));
+    var check = getSelectedCheckboxValues('color');
+    putCheck(check);
+  });
+});
+function putCheck(check) {
+  for (let i = 0; i < check.length; i++) {
+    const element = check[i];
+    if (i == 0) {
+      if (element == 'Screen Reader') {
+        localStorage.setItem('screen_read', 'On');
+        console.log('Cookie made ');
+      } else {
+        localStorage.setItem('screen_read', 'Off');
+        console.log('Cookie made Off');
+      }
+    } else if (i == 1) {
+      if (element == 'Voice Commands') localStorage.setItem('voice_cmd', 'On');
+      else localStorage.setItem('voice_cmd', 'Off');
+    } else if (i == 2) {
+      if (element == 'Gesture Tracking')
+        localStorage.setItem('gesture_trk', 'On');
+      else localStorage.setItem('gesture_trk', 'Off');
+    } else if (i == 3) {
+      if (element == 'Image Video Captioning')
+        localStorage.setItem('img_caption', 'On');
+      else localStorage.setItem('img_caption', 'Off');
+    } else if (i == 4) {
+      if (element == 'Error and Success Messages')
+        localStorage.setItem('error_msg', 'On');
+      else localStorage.setItem('error_msg', 'Off');
+    } else if (i == 5) {
+      if (element == 'PDF generation for Text to Braille Conversion Devices')
+        localStorage.setItem('pdf_gen', 'On');
+      else localStorage.setItem('pdf_gen', 'Off');
+    } else if (i == 6) {
+      if (element == 'Spelling and Meaning Assistance')
+        localStorage.setItem('spell_assist', 'On');
+      else localStorage.setItem('spell_assist', 'Off');
+    } else if (i == 7) {
+      if (element == 'Keyboard Shortcuts')
+        localStorage.setItem('spell_assist', 'On');
+      else localStorage.setItem('spell_assist', 'Off');
+    } else if (i == 8) {
+      if (element == 'Font and Font Size Adjustment')
+        localStorage.setItem('font_adjust', 'On');
+      else localStorage.setItem('font_adjust', 'Off');
+    } else if (i == 9) {
+      if (element == 'Text Spacing') localStorage.setItem('text_spacing', 'On');
+      else localStorage.setItem('text_spacing', 'Off');
+    } else if (i == 10) {
+      if (element == 'Change Color Schemes')
+        localStorage.setItem('change_color', 'On');
+      else localStorage.setItem('change_color', 'Off');
+    } else if (i == 11) {
+      if (element == 'Button and Form Highlighting and Enlargement')
+        localStorage.setItem('button_higlight', 'On');
+      else localStorage.setItem('button_higlight', 'Off');
     }
   }
-  //intro.classList.toggle("hidden");
-}*/
+}
